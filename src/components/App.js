7bf4626fb94f47be456908dc8af4913c962e1bc0
Rotem Slootzky <rotem@soluto.com>
2017-05-16 23:54:35 +0300

    Prettier

--- src/components/App.js
+++ src/components/App.js
@@ -4,28 +4,26 @@ import Popular from './Popular';
 import * as ReactRouter from 'react-router-dom';
 import Nav from './Nav';
 import Home from './Home';
-import Battle from './Battle'
+import Battle from './Battle';
 
-var Roter = ReactRouter.BrowserRouter;
-var Route = ReactRouter.Route;
-var Switch = ReactRouter.Switch;
-
+const Roter = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+const Switch = ReactRouter.Switch;
 
 class App extends Component {
   render() {
     return (
-    <Roter>
-      <div className="container">
-        <Nav/>
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/battle' component={Battle}/>
-          <Route path='/popular' component={Popular}/>
-          <Route render={()=><p>Not Found</p>}/>
-        </Switch>
-      </div>
-    </Roter>
-
+      <Roter>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/battle" component={Battle} />
+            <Route path="/popular" component={Popular} />
+            <Route render={() => <p>Not Found</p>} />
+          </Switch>
+        </div>
+      </Roter>
     );
   }
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,12 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
-    fetchPopularRepos : async (language) =>{
-        var encodeURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+  fetchPopularRepos: async language => {
+    const encodeURI = window.encodeURI(
+        `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+    );
 
-        var results = await axios.get(encodeURI);
-        return results.data.items;
-    },
-
+    const results = await axios.get(encodeURI);
+    return results.data.items;
+  },
 };
